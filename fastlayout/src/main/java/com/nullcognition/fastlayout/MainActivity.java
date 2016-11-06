e0366229c8053bd75ff631ff3decbff90ffb03bd
eersin <eersin@mmsofts.com>
2016-11-06 22:51:11 +0700

    Issue responded to, attempt test according to response

--- fastlayout/src/main/java/com/nullcognition/fastlayout/MainActivity.java
+++ fastlayout/src/main/java/com/nullcognition/fastlayout/MainActivity.java
@@ -7,8 +7,16 @@ public class MainActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    ActivityMainLayout layout =
-        LayoutCache.getInstance().getLayout(this, LayoutCache.Activity_Main_Layout);
+    ActivityMainLayout layout = LayoutCache.getInstance()
+        .getLayout(this,
+            LayoutCache.Activity_Main_Layout); // loads but crashes on stop/ or is it destroy
+    //new ActivityMainLayout(this); // working, but doesn't cache
+
     setContentView(layout);
+  }
+
+  @Override protected void onDestroy() {
+    // cache should be cleared here
+    super.onDestroy();
   }
 }

62db27c968f33b1d4959fab5d23ad2d5d7363a86
eersin <eersin@mmsofts.com>
2016-11-24 20:11:16 +0700

    Testing tinting with svg, didn't then did work, not sure why but I used a different layout. The error was that I was casting to the wrong type in my findviewbyid, which was refrencing a view that I had changed from PercentRelative to LinearLayout, thus I tried rebuilding and invalidating caches but only the layout change worked.

--- svg/src/main/java/com/nullcognition/svg/MainActivity.java
+++ svg/src/main/java/com/nullcognition/svg/MainActivity.java
@@ -6,10 +6,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 /*
   http://inloop.github.io/svg2android/
   To convert from zeplins svg
+
+  what we have learned is the fill xml tags are mostly api 18+ and dont work with tinting
+  so we tried applying a style with fill color, but that did not work either so we try
+  Emma Guy themeing the fill color directly as the attribute of the fill color of the svg
+
+  set the app:theme and the android:theme to my custome style but it didn't
  */
 
 public class MainActivity extends AppCompatActivity {
@@ -32,6 +40,42 @@ public class MainActivity extends AppCompatActivity {
         actionBar.hide();
       }
     }
-    setContentView(R.layout.activity_main);
+    setContentView(R.layout.test);
+  }
+
+  @Override protected void onResume() {
+    super.onResume();
+    // test alternative way
+
+    LinearLayout ll = (LinearLayout) findViewById(R.id.test);
+    ImageView imageView = (ImageView) findViewById(R.id.cir);
+    ImageView wax = (ImageView) findViewById(R.id.ic_waxxi);
+    ImageView srcd = (ImageView) findViewById(R.id.srcd);
+
+    // working after new layout attempt
+
+    //imageView.setColorFilter(ContextCompat.getColor(this, R.color.colorAccent),
+    //    PorterDuff.Mode.SRC_IN);
+    //DrawableCompat.setTint(imageView.getDrawable(), ContextCompat.getColor(this, R.color.colorAccent));
+    //DrawableCompat.setTintMode(imageView.getDrawable(), PorterDuff.Mode.SRC_IN);
+
+  }
+
+  void emmaGuy() {
+
+    // uses internal apis, may not be suitable
+
+    //final ContextThemeWrapper wrapper = new ContextThemeWrapper(this, R.style.ColorStyle);
+    //final Drawable
+    //    drawable = ResourcesCompat.getDrawable(getResources(), R.drawable.cir, wrapper.getTheme());
+    //imageView.setImageDrawable(drawable);
+    //
+    //
+    //final Resources.Theme theme = getResources().newTheme();
+    //
+    //theme.applyStyle(R.style.ColorStyle, false);
+    //
+    //final Drawable drawable = ResourcesCompat.getDrawable(getResources(), R.drawable.cir, theme);
+    //imageView.setImageDrawable(drawable);
   }
 }

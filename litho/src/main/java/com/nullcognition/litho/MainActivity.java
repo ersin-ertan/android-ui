770d5d71f23ea7013f8c1d89694e4f9199dea5e7
eersin <eersin@mmsofts.com>
2017-04-21 10:46:08 +0700

    Update: - working litho example - update build versions

--- litho/src/main/java/com/nullcognition/litho/MainActivity.java
+++ litho/src/main/java/com/nullcognition/litho/MainActivity.java
@@ -12,10 +12,10 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     //setContentView(R.layout.activity_main);
 
-    ComponentContext cc = new ComponentContext(this);
+    final ComponentContext c = new ComponentContext(this);
 
-    LithoView lithoView =
-        LithoView.create(this, Text.create(cc).text("TEXT").textSizeDip(24).build());
+    final LithoView lithoView = LithoView.create(this /* context */,
+        Text.create(c).text("Hello, World!").textSizeDip(298).build());
 
     setContentView(lithoView);
   }
